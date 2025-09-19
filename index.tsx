6b1fba681f0d7e2564ec86453d53f4c88ca1f179
Carl Nienaber <carlnienaber@Marloises-Mac-mini.local>
2025-09-19 11:05:26 +0200

    Manual Changes

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
-import App from './src/App';
+import App from './App'; // Corrected Path
 import AuthWrapper from './src/components/auth/AuthWrapper';
-import './src/index.css';
+import './index.css'; // Corrected Path
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
